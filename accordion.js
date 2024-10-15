470ce134fec9c4bbd5494693a31b2a0cccfed3df
Tarun <tk7864225@gmail.com>
2024-10-15 21:30:31 +0530

    Add files via upload

---
+++ accordion.js
@@ -0,0 +1,6 @@
+let accordion = document.getElementsByClassName('content-container');
+for(var i=0 ; i<accordion.length ; i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active');
+    });
+}
